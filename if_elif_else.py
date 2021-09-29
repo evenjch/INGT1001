5a0bc180bd9a0d648bd9af70920afd1bab2d4fdb
Even Johan Christiansen <evenjc@ntnu.no>
2021-09-29 13:28:47 +0200

    La til if_elif_else.py

---
+++ if_elif_else.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 13:25:05 2021
+
+@author: evenjc
+
+Skriv ferdig programmet, slik at riktig melding printes ut i forhold til alder.
+
+Dersom alderen er 0-16 skal det stå "Du er jo bare barnet!"
+Er alderen 16-60 skal "Midten av livet, ser jeg!" stå.
+Og over 60 år skal "Oi, slutten av visa?" stå.
+
+"""
+# Spør om alder, og gjør om til integer
+age = int(input("Hvor gammel er du?"))
+
+
+# If-else-struktur for å avgjøre hvilken print som gjelder
+if age .... :
+    print("Du er jo bare barnet!")
+elif age .... :
+    print("Midten av livet, ser jeg!")
+else:
+    print("Oi, slutten av visa?")
