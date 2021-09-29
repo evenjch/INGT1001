9f7c3b3084e8d5ad6b470456c2cd505f71d6e0cb
Even Johan Christiansen <evenjc@ntnu.no>
2021-09-29 13:24:36 +0200

    La til lister.py

---
+++ lister.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 12:25:26 2021
+
+@author: evenjc
+"""
+
+# Man kan definere en tom liste slik
+min_liste = []
+
+# Deretter kan man legge verdier inn i denne listen
+min_liste.append(5)
+
+# Skriv inn kode som legger inn følgende verdier i listen
+# Strengen "a", tallet 45, tallet 56.5 og strengen "hallo"
+???????????
+???????????
+???????????
+
+# Fyll inn for-løkken slik at den printer ut hver verdi i listen
+for ?? in ??:
+    print(??)
+    
+    
+# Man kan også legge elementer inn i listen når man definerer den
+min_nye_liste = [1, 35, "hei", 42]
+
+# En liste kan man enkelt bruke if-setningen for å sjekke om inneholder noe
+if 35 in min_nye_liste:
+    print("35 er i listen!")
+    
+if 4 in min_nye_liste:
+    print("4 er i listen!")
