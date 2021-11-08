8b2c9e57ccb4c87007d6b5b136118e70f67505f3
Even Johan Christiansen <evenjc@ntnu.no>
2021-11-08 11:34:18 +0100

    Added template for RPi assignment, changed date format

---
+++ 08-11-2021/template.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Our first co-op assignment. A template for a program that sequentially 
+runs each group members functions. Remember that pass is just there as a 
+place holder, since there is no code there yet.
+
+@author: INGT1001
+"""
+def bruker1():
+    # Bruker1's function. Replace with pass
+    pass
+
+def bruker2():
+    # Bruker2's function. Replace with pass
+    pass
+
+def main():
+    # Logic for calling all user functions
+    pass
+
+if __name__ == "__main__":
+    main()
